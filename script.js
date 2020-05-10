5a154f27d957c7ffb1a10a16b5301d150cfb5543
vivian-ms <56147408+vivian-ms@users.noreply.github.com>
2020-05-10 15:51:55 -0700

    Updated displayQuote function so quotes fade in/out and replaced quotes with fa quote icons

--- script.js
+++ script.js
@@ -6,23 +6,36 @@ $(function() {
   $('#new-quote').on('click', getRandomQuote);
 });
 
+
 function getQuotes() {
   axios.get('https://type.fit/api/quotes')
     .then(res => {
-      // Get first 100 qutoes
+      // Get first 100 quotes
       quotes = res.data.slice(0, 100);
       getRandomQuote();
     });
 }
+
 
 function getRandomQuote() {
   let i = Math.floor(Math.random() * quotes.length);
   displayQuote(quotes[i]);
 }
 
+
 function displayQuote(quote) {
-  $('#text').text(`"${quote.text}"`);
-  $('#author').text(quote.author);
+  let author = quote.author ?? 'Unknown';
+
+  if (!$('#author').text()) {
+    $('#text').html(`<i class="fa fa-quote-left"></i> ${quote.text} <i class="fa fa-quote-right"></i>`);
+    $('#author').text(author)
+  } else {
+    $('#quote-box').fadeOut(() => {
+      $('#text').html(`<i class="fa fa-quote-left"></i> ${quote.text} <i class="fa fa-quote-right"></i>`);
+      $('#author').text(author);
+      $('#quote-box').fadeIn();
+    });
+  }
 
   let url = encodeURIComponent(`"${quote.text}" ${quote.author}`);
   $('#tweet-quote').attr('href', `https://twitter.com/intent/tweet?text=${url}`);
